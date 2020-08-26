1a476231802070b997bd3ecea56ff7f7c8ad9792
Katerina Chytra <32932363+katerinachytra@users.noreply.github.com>
2020-08-26 15:24:33 +0200

    Add files via upload
    save as .hdf5, simplified destruction

--- Destroy_MNISTdigits.py
+++ Destroy_MNISTdigits.py
@@ -1,57 +1,92 @@
-# Simple code for destroying MNIST digits, 
-# Useful for image restoration tasks when something is missing in the image
-
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import os
+import h5py
 
 
 def destroy(train_x, label_x):
     # null random rows or columns
-    i, j = np.min(train_x.shape),0
-    while i+j >= np.min(train_x.shape):
-        i, j = [item for item in np.random.randint([8,2], [27, 7], size=2)]
-    mask = tf.constant(28*[i*[1.]+j*[0.]+(28-i-j)*[1.]])
-    
-    # null horizontally or or vertically, one always horizontally
-    if np.random.normal(0, 1) >=0 or label_x == 1:
-        mask = tf.transpose(mask)
-
-    destroyed = mask*train_x   # element-wise multiplication
+    i, j = np.min(train_x.shape), 0
+    while i + j >= np.min(train_x.shape):
+        i = np.random.randint(8, 27, size=1)[0] # radek
+        j = np.random.randint(2, 7, size=1)[0] # pocet radku
+    mask = np.ones(train_x.shape)
+    mask[i:i+j, :] = 0
+    # null horizontally or vertically, one always horizontally
+    if np.random.normal(0, 1) >= 0 or label_x == 1:
+        mask = np.transpose(mask)
+    destroyed = mask * train_x  # element-wise multiplication
     return destroyed
 
 
 def save_images(orig, destroyed):
     fig = plt.figure(figsize=(4, 4))
-    for i in range(0,16,2):
-        
-        plt.subplot(4, 4, i+1)
+
+    for i in range(0, 16, 2):
+        plt.subplot(4, 4, i + 1)
         plt.imshow(orig[i, :, :], cmap='gray')
         plt.axis('off')
         plt.title('orig')
-        
+
         plt.subplot(4, 4, i + 2)
+        destroyed = np.asarray(destroyed)
         plt.imshow(destroyed[i, :, :], cmap='gray')
-        plt.axis('off')   
+        plt.axis('off')
         plt.title('destroyed')
-        
     plt.tight_layout()
     plt.savefig('orig-destroyed.png')
 
+
+start_time = time.time()
 # --DATASET----
 digits_mnist = tf.keras.datasets.mnist
-(train_images, train_label), (_,_) = digits_mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = digits_mnist.load_data()
 train_images = train_images.reshape(train_images.shape[0], 28, 28).astype('float32')
 # Normalizing the images to the range of [0., 1.]
 train_images /= 255.
 
-destroys = []
+TRAIN_BUF = 10000 # 10 000
+train_images = train_images[:TRAIN_BUF]
+train_labels = train_labels[:TRAIN_BUF]
 
-for train_x_orig, label in zip(train_images, train_label):
+# create hdf5 file, which will contain all dataset
+f = h5py.File('destroy_dset.hdf5', 'a') # nelze 'a' - musela bych oštřit, aby groups nemely stejna jmena
+
+grp_names = sorted(int(item) for item in list(f.keys()))
+print(grp_names)
+grp_name = int(grp_names[-1])+2
+print(grp_name)
+i = 0
+for train_x_orig, label in zip(train_images, train_labels):
+    start_loop = time.time()
     train_x = destroy(train_x_orig, label)
-    destroys.append(train_x)
+    print(grp_name)
+    grp = f.create_group(str(grp_name))
+    grp.attrs['label'] = label
+    dset1 = grp.create_dataset('destr', data=train_x)
+    dset2 = grp.create_dataset('orig', data=train_x_orig)
+    print('step: ', i)
+    i = i + 1
+    grp_name = grp_name+1
 
-destroys = np.asarray(destroys)
-# prepare dataset for training
-train_dataset = tf.data.Dataset.from_tensor_slices((train_images, destroys))
-save_images(train_images, destroys)
+def printname(name):
+    print(name)
+
+# f.visit(printname) # vypise cely dataset - zrejma struktura
+
+
+end_time = time.time()
+print('Time to elapse the destruction: ', round(end_time - start_time, 3), 's')
+exit()
+print(f['0'])
+dgrp = f['0']
+print(dgrp.attrs['label'])
+dset = dgrp['destr']
+plt.imshow(dset)
+plt.show()
+
+
+# save_images(train_images,destroys)
+
